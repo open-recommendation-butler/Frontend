bbc9a2eb6963826e9e0ddd54b79ed59f6dc0464e
matthmeyer <maetthy@arcor.de>
2023-03-14 23:26:15 +0100

    Adjust imports

--- src/components/searchResults/similarSearchRequests.js
+++ src/components/searchResults/similarSearchRequests.js
@@ -1,12 +1,15 @@
-function SimilarSearchRequests({similar_search_requests}) {
+import Customizations from '../../Customizations';
 
-  if (!similar_search_requests) return;
+function SimilarSearchRequests({results}) {
+
+  if (!results) return;
+  if (!Customizations.settings.ShowSimilarSearches) return;
 
   return (
     <div className="max-w-xl mt-8">
-      <h3 className="text-xl font-bold mb-6">Similar search requests</h3>
+      <h3 className="text-xl font-bold mb-6">{Customizations.literals.similarSearchesTitle}</h3>
       <div className="grid sm:grid-cols-2 gap-3">
-        {similar_search_requests.map((simSearch) => 
+        {results.similar_search_requests.map((simSearch) => 
         <a href={`/search?q=${simSearch}`} key={simSearch}>
           <div className="rounded-full bg-slate-200 py-3 px-4 flex items-center hover:underline hover:bg-slate-300">
               <svg className="mr-4 min-w-[20px]" width="20" height="20" viewBox="0 0 477 448" fill="none" xmlns="http://www.w3.org/2000/svg">
--- src/helpers/calculatePageList.js
+++ src/helpers/calculatePageList.js
@@ -1,17 +1,19 @@
-function calculatePageList(page, hitCount, HITS_PER_PAGE) {
+import Customizations from '../Customizations';
+
+function calculatePageList(page, hitCount) {
   let start = 1;
   let end = 5;
-  if (page < (Math.ceil(hitCount / HITS_PER_PAGE) - 2)) {
+  if (page < (Math.ceil(hitCount / Customizations.settings.HitsPerPage) - 2)) {
     if (page < 3) {
-      if (Math.ceil(hitCount / HITS_PER_PAGE) < 5) {
-        end = Math.ceil(hitCount / HITS_PER_PAGE);
+      if (Math.ceil(hitCount / Customizations.settings.HitsPerPage) < 5) {
+        end = Math.ceil(hitCount / Customizations.settings.HitsPerPage);
       }
     } else {
       end = page + 2;
       start = page - 2;
     }
   } else {
-    end = Math.ceil(hitCount / HITS_PER_PAGE);
+    end = Math.ceil(hitCount / Customizations.settings.HitsPerPage);
     if (page < 5) {
       start = 1;
     } else {
