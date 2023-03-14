0091ce970ea45f01f9979efaeaef6b9da003c6d1
matthmeyer <maetthy@arcor.de>
2023-03-14 11:21:15 +0100

    Add params to useEffect()

--- src/pages/searchPage.js
+++ src/pages/searchPage.js
@@ -54,7 +54,7 @@ function SearchPage() {
       setPublisher(publisher);
       search(q, p, contentType, category, publisher);
     };
-  }, [searchParams]);
+  }, [searchParams, publishers, categories]);
 
   useEffect(() => {
     if (!results) return;
@@ -193,20 +193,20 @@ function SearchPage() {
                     <div className='mt-14'>
                       <a 
                         href={`/search?q=${query}&page=${page - 1}${contentType ? '&content_type=' + contentType : ''}${category ? '&category=' + category : ''}${publisher ? '&publisher=' + publisher : ''}`}
-                        onClick={(e) => page == 1 && e.preventDefault()} 
-                        className={`rounded-l-full border px-3 py-2 font-bold ${page == 1 ? "bg-slate-200 text-gray-500 cursor-not-allowed" : "hover:bg-slate-100 cursor-pointer"}`}
+                        onClick={(e) => page === 1 && e.preventDefault()} 
+                        className={`rounded-l-full border px-3 py-2 font-bold ${page === 1 ? "bg-slate-200 text-gray-500 cursor-not-allowed" : "hover:bg-slate-100 cursor-pointer"}`}
                       >Zurück</a>
                       {pageList.map((site_page) => 
                         <a 
                           href={`/search?q=${query}&page=${site_page}${contentType ? '&content_type=' + contentType : ''}${category ? '&category=' + category : ''}${publisher ? '&publisher=' + publisher : ''}`}
-                          onClick={(e) => page == site_page && e.preventDefault()} 
-                          className={`border-y border-r px-3 py-2 font-bold ${page == site_page ? "bg-slate-200 text-gray-500 cursor-not-allowed" : "hover:bg-slate-100 cursor-pointer"}`}
+                          onClick={(e) => page === site_page && e.preventDefault()} 
+                          className={`border-y border-r px-3 py-2 font-bold ${page === site_page ? "bg-slate-200 text-gray-500 cursor-not-allowed" : "hover:bg-slate-100 cursor-pointer"}`}
                         >{site_page}</a>
                       )}
                       <a 
                         href={`/search?q=${query}&page=${page + 1}${contentType ? '&content_type=' + contentType : ''}${category ? '&category=' + category : ''}${publisher ? '&publisher=' + publisher : ''}`}
-                        onClick={(e) => page == Math.ceil(results.hitCount / HITS_PER_PAGE) && e.preventDefault()} 
-                        className={`rounded-r-full border-y border-r px-3 py-2 font-bold ${page == Math.ceil(results.hitCount / HITS_PER_PAGE) ? "bg-slate-200 text-gray-500 cursor-not-allowed" : "hover:bg-slate-100 cursor-pointer"}`}
+                        onClick={(e) => page === Math.ceil(results.hitCount / HITS_PER_PAGE) && e.preventDefault()} 
+                        className={`rounded-r-full border-y border-r px-3 py-2 font-bold ${page === Math.ceil(results.hitCount / HITS_PER_PAGE) ? "bg-slate-200 text-gray-500 cursor-not-allowed" : "hover:bg-slate-100 cursor-pointer"}`}
                       >Weiter</a>
                     </div>
                   </div>
