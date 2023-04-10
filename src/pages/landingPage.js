5b69fdbdc80055622072a4b56e806f61acd93bdb
matthmeyer <maetthy@arcor.de>
2023-04-10 18:30:21 +0200

    Add new head tagline

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -6,7 +6,7 @@ function LandingPage() {
     <>
       <div className="container mx-auto px-3 h-[90vh] flex">
         <div className='my-auto w-full'>
-          <h1 className="text-3xl font-bold text-center text-slate-800 mb-10">Find what feels missing</h1>
+          <h1 className="text-3xl font-bold text-center text-slate-800 mb-10">Add great search<br />to increase readership</h1>
           <div className="max-w-xl mx-auto">
             <SearchField autoFocus={true} />
           </div>
