024872c14270c9a7846b7aca282a17b93cad2007
matthmeyer <maetthy@arcor.de>
2023-04-11 12:04:58 +0200

    Cahnge backend domain

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -1,14 +1,13 @@
 import axios from 'axios';
 
-// let DOMAIN = "http://localhost:8000/api";
-let DOMAIN = "https://open-recommendation-butler.tech/api";
+let DOMAIN = "http://localhost:8001";
+// let DOMAIN = "https://open-recommendation-butler.tech/api";
 
 export async function GET(path) {
   return axios.get(`${DOMAIN}${path}`)
 }
 
 export async function POST(path, data=null) {
-  let headers = {};
   return axios.post(`${DOMAIN}${path}`, data)
 }
 
