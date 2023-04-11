fb586474ce3b9cd0e8f8759de1b604ec68019d4a
matthmeyer <maetthy@arcor.de>
2023-04-11 23:43:06 +0200

    Adjust url

--- src/helpers/requests.js
+++ src/helpers/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // let DOMAIN = "http://localhost:8001";
-let DOMAIN = "https://api.open-recommendation-butler.tech/api";
+let DOMAIN = "https://api.open-recommendation-butler.tech";
 
 export async function GET(path) {
   return axios.get(`${DOMAIN}${path}`)
